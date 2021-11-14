931ebeef6e8ebcfe35b4f8e56dde0b050e8f8b43
lucky-lxb <13925980658@163.com>
2021-11-14 20:36:03 +0800

    文件上下行接口

--- src/main/java/ApplicationOfComments/StartApplication.java
+++ src/main/java/ApplicationOfComments/StartApplication.java
@@ -1,9 +1,12 @@
 package ApplicationOfComments;
 
+import ApplicationOfComments.Util.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class StartApplication {
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class,args);
