13538affa827bc51ba4cc9a9cb25a27bbb4bb735
lucky-lxb <13925980658@163.com>
2021-11-15 13:07:28 +0800

    看不懂的spring reactor系列

--- out/artifacts/spring_with_APIs_Web_exploded/src/main/java/ApplicationOfComments/StartApplication.java
+++ out/artifacts/spring_with_APIs_Web_exploded/src/main/java/ApplicationOfComments/StartApplication.java
@@ -1,11 +1,24 @@
 package ApplicationOfComments;
 
+import ApplicationOfComments.Service.StorageService;
+import ApplicationOfComments.Util.StorageProperties;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class StartApplication {
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class,args);
+    }
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 }
